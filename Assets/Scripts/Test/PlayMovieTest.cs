23a1c20744cc531642784782789bf31b82c438f1
kuertian <kuertian@qq.com>
2021-02-20 21:01:25 +0800

    test

---
+++ Assets/Scripts/Test/PlayMovieTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMovieTest : MonoBehaviour
+{
+    public GameObject gameObj;
+    public GameObject RawImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayMovie()
+    {
+        gameObj.SetActive(true);
+        RawImage.SetActive(true);
+    }
+    public void CloseMovie()
+    {
+        gameObj.SetActive(false);
+        RawImage.SetActive(false);
+    }
+}
